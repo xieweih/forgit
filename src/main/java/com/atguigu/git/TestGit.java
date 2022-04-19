cd9205c73853855488c895b06caec498a1193e1c
xieweih <1741237580@qq.com>
2022-04-19 10:55:27 +0800

    idea first git commit

---
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @author 谢炜宏
+ * @version 1.0
+ */
+public class TestGit {
+    public void say(){
+        System.out.println("hehe");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
---
+++ src/test/java/com/atguigu/git/TestGitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author 谢炜宏
+ * @version 1.0
+ */
+class TestGitTest {
+    @org.junit.Test
+   public void TestGitTest(){
+       TestGit testGit = new TestGit();
+        testGit.say();
+   }
+}
