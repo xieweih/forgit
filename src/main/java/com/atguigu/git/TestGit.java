4deadcc5feca38bd410f3eaf1fe84750d890eb98
xieweih <1741237580@qq.com>
2022-04-19 11:22:00 +0800

    hotfix commit test conflict

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -14,5 +14,6 @@ public class TestGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hot-fix commit");
+        System.out.println("hotfix commit test conflict");
     }
 }
