8e6b5815243035aacfa3754860d9f0aef0a9ee6f
xieweih <1741237580@qq.com>
2022-04-19 11:06:24 +0800

    idea third git commit

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -12,5 +12,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
